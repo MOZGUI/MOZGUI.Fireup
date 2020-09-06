e16743cb17eb39d40a800d3ed8c7973155ed62f3
Беляков Павел <belyakov.p.i@gmail.com>
2020-09-06 12:59:00 +0300

    first code added

---
+++ MOZGUI.Fireup/App.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+
+namespace MOZGUI.Fireup
+{
+
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            try
+            {
+                #if DEBUG
+                       MessageBox.Show("This message displayed for debug version");
+                #endif
+
+                if (e.Args != null && e.Args.Count() > 0)
+                {
+                    var argument = e.Args[0];
+                    Uri uri = new Uri(argument);
+
+                    if (uri.Scheme == "mozgui-fireup")
+                    {
+                        var localPath = uri.LocalPath;
+
+                        var process = new Process
+                        {
+                            StartInfo = new ProcessStartInfo(localPath)
+                            {
+                                UseShellExecute = true,
+                                CreateNoWindow = false
+                            }
+                        };
+                        process.Start();
+                    }
+                    else
+                    {
+
+                    }
+                }
+                else
+                {
+                    base.Shutdown();
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+            base.Shutdown();
+        }
+    }
+}
